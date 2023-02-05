e91aef627904aec9f70b79dd0ad88b835c8ccc34
IdanHamo <97957689+IdanHamo@users.noreply.github.com>
2023-02-05 15:38:25 +0200

    .

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/global/navbar";
+import Footer from "./components/global/footer";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="App flex flex-col min-h-screen">
+      <header>
+        <Navbar></Navbar>
+      </header>
+      <div className="grow"></div>
+      <footer>
+        <Footer></Footer>
+      </footer>
     </div>
   );
 }
